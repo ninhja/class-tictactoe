556a6b916a6d93dcf0ec00b5e3937507eed49056
ninhja <ariannaninh@gmail.com>
2024-04-25 14:44:36 -0700

    refactored Board to render squares using 2 loops rather than hardcoding

--- src/App.jsx
+++ src/App.jsx
@@ -35,21 +35,17 @@ function Board({ xIsNext, squares, onPlay }) {
 
   return (
     <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {[...Array(3).keys()].map((i) => (
+        <div className="board-row" key={i}>
+          {[...Array(3).keys()].map((j) => (
+            <Square
+              value={squares[i * 3 + j]}
+              onSquareClick={() => handleClick(i * 3 + j)}
+              key={j}
+            />
+          ))}
+        </div>
+      ))}
       <div className="status">{status}</div>
     </>
   );
@@ -120,6 +116,7 @@ function calculateWinner(squares) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
+      // return { player: squares[a], line: [a, b, c] };
     }
   }
   return null;
